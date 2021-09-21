2ab0f2580926cee45c22eb98dd13bf5d0848b30b
Mark L-M <mark.lohsemiranda@gmail.com>
2021-09-20 19:38:09 -0700

    first update

---
+++ script.js
@@ -0,0 +1,18 @@
+let gamesPlayed = 0;
+let cpuScore = 0;
+let playerScore = 0;
+
+function start() {
+let initialAnswer = confirm("SHALL WE PLAY A GAME?");
+    if (initialAnswer) {
+        alert("you said okay");}
+    else {
+        alert('you said cancel');
+    };   
+};
+
+
+
+
+
+
